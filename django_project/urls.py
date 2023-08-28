5b73c3f9b5229c787290f7ea66a6a01a2c38489b
odinakholiqov <bobojon.kholikov@gmail.com>
2023-08-28 16:36:16 +0500

    ch6

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import django.contrib.auth.urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # user management
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")), 
     path("", include("pages.urls")),
 ]
