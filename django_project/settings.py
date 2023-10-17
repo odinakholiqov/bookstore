fd9cc9415fc01921e986944f5ab24d7f804e6834
odinakholiqov <bobojon.kholikov@gmail.com>
2023-10-17 13:58:53 +0500

    ch9

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = Env()
+env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)^vcpo3)3z^c19(xb^ht4s9k-uyo6u^hhj((q7-#-*84$nie^1"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+DEBUG = env.bool("DJANGO_DEBUG")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -88,6 +91,12 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    "default": env.dj_db_url("DATABASE_URL", 
+                             default="postgres://postgres@db/postgres")
+}
+
+"""
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
@@ -97,6 +106,7 @@ DATABASES = {
         "PORT": 5432,  # default postgres port
     }
 }
+"""
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
